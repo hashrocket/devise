4ce89e145fc23484e8ba27cc30b13facc0bb57ab
Pairing Workstation Bender <dev+bender@hashrocket.com>
2010-01-21 14:32:25 -0500

    Add facebook_connectable to default strategies test

--- test/devise_test.rb
+++ test/devise_test.rb
@@ -25,7 +25,7 @@ class DeviseTest < ActiveSupport::TestCase
     Devise.configure_warden(config)
 
     assert_equal Devise::FailureApp, config.failure_app
-    assert_equal [:rememberable, :authenticatable], config.default_strategies
+    assert_equal [:rememberable, :authenticatable, :facebook_connectable], config.default_strategies
     assert_equal :user, config.default_scope
     assert config.silence_missing_strategies?
   end
